7fd7dccc5aa962e4dda1f41cf1c80ce2e5154bd6
Pau Campos <pau.campos89@gmail.com>
2022-10-25 00:04:00 -0300

    Schema mongoose

---
+++ src/models/Ficha.js
@@ -0,0 +1,51 @@
+import { Schema, model} from "mongoose";
+
+const fichaSchema = new Schema({
+    rut: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    nombres: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    apellidos: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    direccion: {
+        type: String,
+        required: true
+    },
+    ciudad: {
+        type: String,
+        required: true
+    },
+    fono: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },  
+    fechaNacimiento: {
+        type: Date,
+        required: true
+    },
+    estadoCivil: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    comentarios: String 
+}, {
+    timestamps: true,
+    versionKey: false
+})
+
+export default model('Ficha', fichaSchema);
